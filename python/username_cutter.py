05de30172a98a6a717f65db531a6af35fc85867b
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-05-28 14:39:27 -0400

    Add files via upload

--- python/username_cutter.py
+++ python/username_cutter.py
@@ -5,7 +5,6 @@ import pandas as pd
 import glob
 import os
 
-#--------------------------- Read in bunch of csv files together
 #read the multiple csv file where only first one keeps the header, rest are pushed below, -1 header
 
 interesting_files = glob.glob("RawCSV_firstBatch/*.csv") #please update the path accordingly! eg. folder/*.csv
@@ -15,12 +14,18 @@ df_list = []
 
 #parse each file, but only select the username columns, and keep stacking them (much effiecient)
 for filename in sorted(interesting_files):
-    df_list.append(pd.read_csv(filename, usecols=['screenNames']))
+    df_list.append(pd.read_csv(filename, usecols=['user_screen_name', 'lang']))
 
 #join all the list
 full_df = pd.concat(df_list)
 
-#------------------------------- Drop the duplicates
+#Modify the full_df to only keep en lang account
+full_df= full_df.loc[full_df['lang'] == 'en']
+
+#drop the lang column from full_df
+full_df= full_df.drop('lang', 1)
+
+#-------------------------------
 
 #we will clear the file where we only keep the unique entries
 
@@ -28,9 +33,9 @@ full_df = pd.concat(df_list)
 #print(full_df.shape) #check pre clean rows output: (rows, column)
 
 # keep only unique usernames
-full_df = full_df.drop_duplicates('screenNames')
+full_df = full_df.drop_duplicates('user_screen_name')
 
-full_df.to_csv('output.csv', sep=',', index= False) #you can name your file whatever you want
+full_df.to_csv('mihir_output_NEW.csv', sep=',', index= False) #you can name your file whatever you want
 
 ##now very important to release the RAM mem back (do this only if you uncommented 52-58)
 ##del full_df
@@ -38,17 +43,14 @@ full_df.to_csv('output.csv', sep=',', index= False) #you can name your file what
 ## print('post-clean: ')
 ## print(full_df.shape) #check post clean rows output: (rows, column)
 
-#------------------------------ manually cut the files to your liking
+#------------------------------
 
 #now we will cut the final file by rows until last row is reached
-
+#
 # file_dir = os.path.dirname(os.path.abspath(__file__))
-# csv_folder = 'cleanusrnms_firstbatch'  # name the sub-directory
+# csv_folder = 'cleanusrnms_firstbatch'
 # os.makedirs(csv_folder) #creates a folder for you automatically
 # csv_folder = csv_folder + '/'
-
-#set chunksize in the second argument
-# for i,chunk in enumerate(pd.read_csv('output.csv', chunksize= 17200)): 
+#
+# for i,chunk in enumerate(pd.read_csv('output.csv', chunksize= 17200)):
 #     chunk.to_csv(csv_folder+'chunk{}.csv'.format(i), index=False)
-
-#EOF
