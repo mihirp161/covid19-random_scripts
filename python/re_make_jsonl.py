ffa2d735c59629bf11a53793555c30b01d76072e
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-06-08 09:14:42 -0400

    Add files via upload

--- python/re_make_jsonl.py
+++ python/re_make_jsonl.py
@@ -6,9 +6,10 @@ import io
 import glob
 import os
 
-
+#Optional, use only if you need to fix the jsonl files for csv conversion + fix used clean_json_step1.py
 #re-rewrites all the files into jsonl format
 results = []
+# give path name to a directory, it will write back a BOM file
 for f in glob.glob("folder_with_all_jsonl/*.jsonl"):
     with open(f, 'r', encoding='utf-8-sig') as infile:
         for jline in infile:
