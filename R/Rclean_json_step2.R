26bd1e529da757e3439685a98820d1444afd84bc
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-06-11 13:46:16 -0400

    Add files via upload

--- R/Rclean_json_step2.R
+++ R/Rclean_json_step2.R
@@ -1,5 +1,9 @@
 #For USF SAIL
 #~Mihir
+
+#For USF SAIL
+#~Mihir
+
 library(readr)
 library(dplyr)
 
@@ -22,7 +26,7 @@ if(dir.exists(out_fold_name)){
   print("Directory exists. Please change both folders names in this block to whatever.")
   quit()
 }else{
-  dir.create(out_fold_name) 
+  dir.create(out_fold_name)
 }
 
 #-----------------------------------------------------------#
@@ -44,17 +48,95 @@ Rand100K_tbl <- lapply(files, readr::read_csv) %>% dplyr::bind_rows()
 
 #--->First, filter the main csv based on the 100K file. We want to keep the user who have been processed though botometer
 csv_tbl_filtered <- csvs_tbl %>%
-                     dplyr::filter(user_screen_name %in% Rand100K_tbl$user_screen_name)
+                      dplyr::filter(user_screen_name %in% Rand100K_tbl$user_screen_name)
 
-#-----X remove the variables we are not using
+#---------------------------------X remove the variables we are not using
 rm(csvs_tbl,Rand100K_tbl)
 
 #--->Second, attach the botometer df to the filtered one, keep the ones that returned NA in Botometer
 
 csv_tbl_filtered_w_bot <- dplyr::left_join(csv_tbl_filtered, bots_tbl, by= "user_screen_name") # Or inner_join??
 
-#-----X remove furthervariable
+#--------------------------------X remove furthervariable
 rm(csv_tbl_filtered,bots_tbl)
 
-#Finall write to a file 
+#--------------------------------X Here you can select the columns you need before writing
+
+#comment out the columns we don't need
+select_columns_here <- c(
+                          "tweet_created_at",              
+                          "tweet_id_str",                     
+                          "tweet_text",                       
+                          "in_reply_to_status_id_str",        
+                          "in_reply_to_user_id_str",          
+                          "in_reply_to_screen_name",
+                          "tweets_retweet_count",
+                          "tweets_favourite_count",
+                          "retweet_created_at",
+                          "retweet_id_str",
+                          "retweet_text",
+                          "retweets_retweet_count",
+                          "retweets_favourite_count",
+                          "user_id_str",
+                          "user_screen_name",
+                          "user_description",
+                          "user_location",
+                          "user_coordinates",
+                          "user_followers_count",
+                          "user_friends_count",
+                          "user_listed_count",
+                          "user_statuses_count",
+                          "user_favourites_count",
+                          "user_profile_creation_at",
+                          "retweets_user_id_str",
+                          "retweets_user_screen_name",
+                          "retweets_user_description",
+                          "retweets_user_location",           
+                          "retweets_user_coordinates",        
+                          "retweets_user_followers_count",    
+                          "retweets_user_friends_count",      
+                          "retweets_user_listed_count",       
+                          "retweets_user_statuses_count",     
+                          "retweets_user_favourites_count",   
+                          "retweets_user_retweet_count",      
+                          "retweets_user_profile_creation_at",
+                          "mentions_screen_names",            
+                          "mentions_user_id_str",             
+                          "retweet_mentions_screen_names",    
+                          "retweet_mentions_user_id_str",     
+                          "quoted_mentions_screen_names",     
+                          "quoted_mentions_user_id_str",      
+                          "quoted_created_at",                
+                          "quoted_id_str",                    
+                          "quoted_text",                      
+                          "quoted_retweet_count",             
+                          "quoted_favourite_count",           
+                          "quoted_user_id_str",               
+                          "quoted_user_screen_name",          
+                          "quoted_user_description",          
+                          "quoted_user_location",             
+                          "quoted_user_coordinates",          
+                          "quoted_user_followers_count",      
+                          "quoted_user_friends_count",        
+                          "quoted_user_listed_count",         
+                          "quoted_user_favourites_count",     
+                          "quoted_user_statuses_count",       
+                          "user_verified",                    
+                          "retweet_user_verified",            
+                          "quoted_user_verified",             
+                          "user_place_name",                  
+                          "retweet_user_place_name",          
+                          "quote_user_place_name",            
+                          "user_country_code",                
+                          "retweet_user_country_code",        
+                          "quote_user_country_code",          
+                          "display_scores_english",           
+                          "display_scores_universal",         
+                          "cap_english",                      
+                          "cap_universal"
+                        )
+
+csv_tbl_filtered_w_bot <- csv_tbl_filtered_w_bot[ ,select_columns_here]
+
+#Finall write to a file
 readr::write_excel_csv(csv_tbl_filtered_w_bot, paste0(out_fold_name,"/", out_csv_name,"_utf8.csv"))
