d3501eede57929a92912c3afb615580b71269e2d
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-07-23 15:01:30 -0400

    Add files via upload

--- R/annotation_cut_prep.R
+++ R/annotation_cut_prep.R
@@ -1,11 +1,9 @@
-# FOR USF SAIL
-# ~Mihir
 library(data.table)
 library(tidyverse)
 
 #Read in the csv file
 # set the path of where the csv files are and end the file path with / character. In windows swap \ with /
-setwd('./BOT_JUNE2_READY/')
+setwd('C:\\Users\\ThinkPad\\Desktop\\July Botometer USF DATA 1\\july mihir botometer')
 
 #filenames
 files <- list.files( pattern="*.csv$")
@@ -18,7 +16,7 @@ setwd('..')
 #This work uses botometer files created between June 15-30 (Botometer ran on 07-03 to 07-05)
 
 #formula
-# 1. use the most recent twitter accounts collected (perhaps between June 15-June 30)
+# 1. use the most recent twitter accounts collected (perhaps between July 1-July 14)
 # 2. using the botometer score, sample 500 accounts following the formula:
 #   a. 40% from CAP >0.8
 #   b. 40% from CAP <0.4
@@ -34,7 +32,7 @@ all_4 <- data %>%
 all_in_4_and_8 <- data %>%
                     dplyr::filter(cap_english >= 0.4 & cap_english <= 0.8)
 
-
+#randomised the placements
 more_than_8 <- all_8[sample(.N, ceiling(0.40*nrow(all_8)), replace = F)]
   
 less_than_4 <-  all_4[sample(.N, ceiling(0.40*nrow(all_4)), replace = F)]
@@ -45,11 +43,17 @@ between_4_and_8 <- all_in_4_and_8[sample(.N, ceiling(0.20*nrow(all_in_4_and_8)),
 set.seed(786)
 rbinded_dfs <- dplyr::bind_rows(more_than_8,less_than_4,between_4_and_8)
 
+#add the urls for profile (some may get suspended or change their names)
+rbinded_dfs$profile_url <- paste0("https://twitter.com/", rbinded_dfs$user_screen_name)
+
 shuffled_rows <- sample(nrow(rbinded_dfs), replace = F)
 rbinded_dfs <- rbinded_dfs[shuffled_rows, ]
 
-sam_500 <- rbinded_dfs[1:500,]
+sam_ndf <- rbinded_dfs[1:20,]
+sam_ndf <- sam_ndf %>% dplyr::select(user_screen_name, profile_url)
 
-readr::write_excel_csv(rbinded_dfs,"june_people_full_randomised.csv")
+#subtract the people who have been picked
 
-readr::write_excel_csv(sam_500,"june_people_for_annotation.csv")
+readr::write_excel_csv(rbinded_dfs,"july_people_full_randomised.csv")
+
+readr::write_excel_csv(sam_ndf,"july_people_for_annotation_sample.csv")
