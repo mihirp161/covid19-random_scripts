609686b6e0cad7bce1638d1af14fbb901755fe79
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-06 13:44:54 -0400

    Add files via upload

--- R/adjaceny_list_csv_maker_doBots.R
+++ R/adjaceny_list_csv_maker_doBots.R
@@ -3,66 +3,86 @@
 
 library(readr)
 library(data.table)
-#library(igraph)
-#library(rgexf)
 library(dplyr)
 
 #-------------------------------------------------- read botometer scores ---------------------------------------
-# read the target files
-setwd('./BOT_CSV_USC_March_Target/')
+# read the portion files
+setwd('C:\\Users\\ThinkPad\\Desktop\\New folder (5)\\march_set_edLi\\portions')
 
 #filenames 
 files <- list.files( pattern="*.csv$")
 
 #read only columns that we want
 temp <- lapply(files, function(x) readr::read_csv(x))
-target_bot <- data.table::rbindlist(temp, fill = T) #make a dt
+full_portions <- data.table::rbindlist(temp, fill = T) #make a dt
+full_portions <- full_portions[!duplicated(full_portions$user_screen_name), ]
 
-colnames(target_bot) <- c("Target", "display_scores_english.Tar", "display_scores_universal.Tar",
-                          "cap_english.Tar", "cap_universal.Tar")
-setwd('..')
-
-# read the source files
-setwd('./BOT_CSV_USC_March_Source/')
+# read the scores files
+setwd('C:\\Users\\ThinkPad\\Desktop\\New folder (5)\\march_set_edLi\\scores')
 
 #filenames 
 files <- list.files( pattern="*.csv$")
 
 #read only columns that we want
 temp <- lapply(files, function(x) readr::read_csv(x))
-source_bot <- data.table::rbindlist(temp, fill = T) #make a dt
+scores <- data.table::rbindlist(temp, fill = T) #make a dt
 
-colnames(source_bot) <- c("Source", "display_scores_english.Sou", "display_scores_universal.Sou",
-                          "cap_english.Sou", "cap_universal.Sou")
+# average the bot score fluctuation if repeated users are present due to cycle
+scores <- scores[ ,.(display_scores_english= weighted.mean(display_scores_english, na.rm=T),
+                     display_scores_universal=weighted.mean(display_scores_universal, na.rm=T),
+                     cap_english=weighted.mean(cap_english,na.rm=T),
+                     cap_universal=weighted.mean(cap_universal,na.rm=T)),by= user_screen_name]
 
-setwd('..')
+# cut the scores dt with >0.5 cap_english
+scores <- scores[scores$cap_english >0.5, ]
+
+#first trim with full_proportion
+full_portions <- full_portions[(full_portions$user_screen_name %in% scores$user_screen_name), ]
+
+#cut the dataframe
+full_portions <- full_portions[ ,2:3]
+
+setwd('C:\\Users\\ThinkPad\\Desktop\\New folder (5)')
+
 #-------------------------------------------------- read the edglist file ---------------------------------------
 
-edgelist_file <- data.table:::fread("edgeList_march_ab6_3k.csv")
-
-x <- data.table:::fread("edgeList_march_ab6_3k.csv")
+edgelist_file <- data.table:::fread("csv_edgeList_march_ab6.csv")
 
 rm(temp)
 #-------------------------------------------------- attach the scores & target to original edgelist ------------
 
+#source only df
+so_df <- full_portions[full_portions$category=="Source", ]
 
-#average the bot score fluctuation
-source_bot <- source_bot[ ,.(display_scores_english.Sou= weighted.mean(display_scores_english.Sou, na.rm=T),
-                             display_scores_universal.Sou=weighted.mean(display_scores_universal.Sou, na.rm=T), 
-                             cap_english.Sou=weighted.mean(cap_english.Sou,na.rm=T),
-                             cap_universal.Sou=weighted.mean(cap_universal.Sou,na.rm=T)),by= Source]
-
-target_bot <- target_bot[ ,.(display_scores_english.Tar= weighted.mean(display_scores_english.Tar, na.rm=T),
-                             display_scores_universal.Tar=weighted.mean(display_scores_universal.Tar, na.rm=T), 
-                             cap_english.Tar=weighted.mean(cap_english.Tar,na.rm=T),
-                             cap_universal.Tar=weighted.mean(cap_universal.Tar,na.rm=T)),by= Target]
+#target only df
+tar_df <- full_portions[full_portions$category=="Target", ]
 
 
-# cut the source dt with >0.5 cap_english
-source_bot <- source_bot[source_bot$cap_english.Sou >0.5, ]
+#now trim the edgelist
+edgelist_file <- edgelist_file[(edgelist_file$Source %in% so_df$user_screen_name), ]
 
-# cut the target dt with >0.5 cap_english
-target_bot <- target_bot[target_bot$cap_english.Tar >0.5, ]
+# #then trim the edgelist
+# edgelist_file <- edgelist_file[(edgelist_file$Target %in% tar_df$user_screen_name), ]
+
+readr::write_excel_csv(edgelist_file, "march_edgelist_botometer_filter_mask_onSource.csv")
+
+# #average the bot score fluctuation
+# source_bot <- source_bot[ ,.(display_scores_english.Sou= weighted.mean(display_scores_english.Sou, na.rm=T),
+#                              display_scores_universal.Sou=weighted.mean(display_scores_universal.Sou, na.rm=T), 
+#                              cap_english.Sou=weighted.mean(cap_english.Sou,na.rm=T),
+#                              cap_universal.Sou=weighted.mean(cap_universal.Sou,na.rm=T)),by= Source]
+# 
+# target_bot <- target_bot[ ,.(display_scores_english.Tar= weighted.mean(display_scores_english.Tar, na.rm=T),
+#                              display_scores_universal.Tar=weighted.mean(display_scores_universal.Tar, na.rm=T), 
+#                              cap_english.Tar=weighted.mean(cap_english.Tar,na.rm=T),
+#                              cap_universal.Tar=weighted.mean(cap_universal.Tar,na.rm=T)),by= Target]
+# 
+# 
+# # cut the source dt with >0.5 cap_english
+# source_bot <- source_bot[source_bot$cap_english.Sou >0.5, ]
+# 
+# # cut the target dt with >0.5 cap_english
+# target_bot <- target_bot[target_bot$cap_english.Tar >0.5, ]
 
 
 #join, it automatically drops the NA
@@ -73,22 +93,11 @@ target_bot <- target_bot[target_bot$cap_english.Tar >0.5, ]
 # form a new edgelist
 
 #first trim with SOURCE
-edgelist_file <- edgelist_file[(edgelist_file$Source %in% source_bot$Source), ]
+#edgelist_file <- edgelist_file[(edgelist_file$Source %in% source_bot$Source), ]
 
 #then trim with SOURCE
 #edgelist_file <- edgelist_file[(edgelist_file$Target %in% target_bot$Target), ]
 
 
-readr::write_excel_csv(edgelist_file, "march_edgelist_botometer_filter.csv")
-
-#-------------------------------------------------------------------------------------------------------------------
-edgelist_file_ori <- data.table:::fread("csv_edgeList_march_ab6.csv")
-
-# edgelist_file[!duplicated(edgelist_file$Source), ]
-
-
-
-
-
-
+# readr::write_excel_csv(edgelist_file, "march_edgelist_botometer_filter.csv")
 
