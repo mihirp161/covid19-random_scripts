0cbf0594ef9c16c41aa41f3d404cecbcaa5c185a
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-10 00:15:12 -0400

    Add files via upload

--- R/nic_fix.R
+++ R/nic_fix.R
@@ -8,29 +8,48 @@ library(stringr)
 library(stringi)
 
 #---------------------------------- read the file---------------------------------------
-this_file <- "drnic_reop_hasReop_march.csv"
 
-df <- readr::read_csv(this_file)
+this_path <- 'C:\\Users\\ThinkPad\\Downloads\\Conspiracy Theory-selected\\fixThese'
+setwd(this_path)
+
+#filenames
+files <- list.files( pattern="*.csv$")
+
+#read only columns that we want
+temp <- lapply(files, function(x) readr::read_csv(x))
+
+file_i <- 1
+
+for(t in temp){
+  
+  df <- t
 
 #----------------------------------- all the fixes-------------------------------------
-#attach hashtags
-df$hashtags <- sapply(qdapRegex::rm_hash(df$complete_texts, extract = T), paste0, collapse=";")
-
-#replace "NA" with NA
-df$hashtags <- gsub('NA', NA, df$hashtags)
-
-
-#attach mentions by grouping all the texts
-df$mentions_screen_name <- sapply(stringr::str_extract_all(df$complete_texts,
-                                  "@[[:alnum:]_]+"), paste0, collapse=";")
-
-#replace " " with NA
-df$mentions_screen_name <- gsub(' ', NA, df$mentions_screen_name)
-
-#remove the last instance of ";" with "" in mention id_str
-df$mentions_id_str <- stringi::stri_replace_last_fixed(df$mentions_id_str , ';', '')
-
-#overwrite
-readr::write_excel_csv(df, this_file)
+  #attach hashtags
+  df$hashtags <- sapply(qdapRegex::rm_hash(df$complete_texts, extract = T), paste0, collapse=";")
+  
+  #replace "NA" with NA
+  df$hashtags <- gsub('NA', NA, df$hashtags)
+  
+  
+  #attach mentions by grouping all the texts
+  df$mentions_screen_name <- sapply(stringr::str_extract_all(df$complete_texts,
+                                    "@[[:alnum:]_]+"), paste0, collapse=";")
+  
+  #replace " " with NA
+  df$mentions_screen_name <- gsub(' ', NA, df$mentions_screen_name)
+  
+  #remove the last instance of ";" with "" in mention id_str
+  df$mentions_id_str <- stringi::stri_replace_last_fixed(df$mentions_id_str , ';', '')
+  
+  setwd('..')
+  
+  #overwrite
+  readr::write_excel_csv(df, files[file_i])
+  
+  file_i <- file_i+1 #increment the file name
+  
+  setwd(this_path)
+}
 
 #EOF
