8ec1f62160adb4735505a64c67ca800e7d4a718e
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-15 15:25:34 -0400

    Add files via upload

--- R/annotation_cut_prep.R
+++ R/annotation_cut_prep.R
@@ -1,22 +1,30 @@
+# SAIL Labs
+# ~Mihir
+
+## Description:
+##-------------
+#* This file was used to give twitter accounts to annotators based on certain 
+#*  formula. This file needs a botometer scores as well as the human led annotations.
+##-------------
+
 library(data.table)
 library(tidyverse)
 
 #Read in the csv file
 # set the path of where the csv files are and end the file path with / character. In windows swap \ with /
-setwd('C:\\Users\\ThinkPad\\Desktop\\July Botometer USF DATA 1\\july mihir botometer')
+setwd('/files_path_here/')
 
 #filenames
 files <- list.files( pattern="*.csv$")
 
-#read only columns that we want
+#read files
 temp <- lapply(files, function(x) readr::read_csv(x))
 data <- data.table::rbindlist(temp, fill = T) #make a df
 
 setwd('..')
-#This work uses botometer files created between June 15-30 (Botometer ran on 07-03 to 07-05)
 
 #----------------------
-#formula
+#formula (OLD)
 # 1. use the most recent twitter accounts collected (perhaps between July 1-July 14)
 # 2. using the botometer score, sample 500 accounts following the formula:
 #   a. 40% from CAP >0.8
@@ -28,7 +36,7 @@ setwd('..')
 # all_8 <- data %>%
 #           dplyr::filter(cap_english > 0.8)
 # 
-# all_4 <- data %>%
+# all_5 <- data %>%
 #           dplyr::filter(cap_english < 0.4)
 # 
 # all_in_4_and_8 <- data %>%
@@ -37,7 +45,7 @@ setwd('..')
 # #randomised the placements
 # more_than_8 <- all_8[sample(.N, ceiling(0.40*nrow(all_8)), replace = F)]
 # 
-# less_than_4 <-  all_4[sample(.N, ceiling(0.40*nrow(all_4)), replace = F)]
+# less_than_4 <-  all_5[sample(.N, ceiling(0.40*nrow(all_5)), replace = F)]
 # 
 # between_4_and_8 <- all_in_4_and_8[sample(.N, ceiling(0.20*nrow(all_in_4_and_8)), replace = F)]
 # 
@@ -46,16 +54,16 @@ setwd('..')
 # rbinded_dfs <- dplyr::bind_rows(more_than_8,less_than_4,between_4_and_8)
 
 #----------------------
-# new formula
+# Current formula (NEW)
 
-# a. 60% from >= 0.8
-# b. 40% from <0.8 
+# a. 50% from >= 0.8
+# b. 50% from <0.8 
 
-setwd('C:\\Users\\ThinkPad\\Desktop\\July Botometer USF DATA 1')
+setwd('/file_path_here/')
 
-data <- data.table::fread("july_people_for_annotation_10P_run3_special.csv")
+data <- data.table::fread("read_annotatio_file.csv")
 
-# OR------------
+#----OR, use portion below if multiple files------------
 #files <- list.files( pattern="*.csv$")
 
 #read only columns that we want
@@ -64,16 +72,16 @@ data <- data.table::fread("july_people_for_annotation_10P_run3_special.csv")
 
 #setwd('..')
 
-all_6 <- data %>%
+all_5.a <- data %>%
           dplyr::filter(cap_english >= 0.8)
 
-all_4 <- data %>%
+all_5.b <- data %>%
           dplyr::filter(cap_english < 0.8)
 
 #randomised the placements
-above_8 <- all_6[sample(.N, ceiling(0.50*nrow(all_6)), replace = F)]
+above_8 <- all_5.a[sample(.N, ceiling(0.50*nrow(all_5.a)), replace = F)]
 
-below_8 <-  all_4[sample(.N, ceiling(0.50*nrow(all_4)), replace = F)]
+below_8 <-  all_5.b[sample(.N, ceiling(0.50*nrow(all_5.b)), replace = F)]
 
 #shffle the rows randomly withou replacement
 set.seed(786)
@@ -88,25 +96,28 @@ rbinded_dfs <- dplyr::bind_rows(above_8[sample(.N, ceiling(0.50*60), replace = F
 rbinded_dfs$profile_url <- paste0("https://twitter.com/", rbinded_dfs$user_screen_name)
 
 
-# shuffled_rows <- sample(nrow(rbinded_dfs), replace = F)
+# shuffled_rows <- sample(nrow(rbinded_dfs), replace = F) #uncomment this when needed (OLD)
 # rbinded_dfs <- rbinded_dfs[shuffled_rows, ]
 # 
 # sam_ndf <- rbinded_dfs[1:20,]
-
+#
 #sam_ndf <- sam_ndf %>% dplyr::select(user_screen_name, profile_url, cap_english)
 
+# resize the dataframe
 sam_ndf <- rbinded_dfs %>% dplyr::select(user_screen_name, profile_url, cap_english)
 
-
+#rename columns
 names(sam_ndf)[names(sam_ndf) == 'user_screen_name'] <- 'ScreenName'
 names(sam_ndf)[names(sam_ndf) == 'profile_url'] <- 'URL'
 
+#shuffle the row placements
 shuffled_rows <- sample(nrow(sam_ndf), replace = F)
 sam_ndf <- sam_ndf[shuffled_rows, ]
 
-#subtract the people who have been picked
+#make distributatble file
+readr::write_excel_csv(sam_ndf,"sample_set_for_distribution.csv")
 
-readr::write_excel_csv(sam_ndf,"july_people_for_annotation_10P_run3_special.csv")
+#subtract the people who have been picked be overwriting the file
+readr::write_excel_csv(total_dfs,"full_file.csv")
 
-readr::write_excel_csv(total_dfs,"july_people_full_run3.csv")
-
+#EOF
