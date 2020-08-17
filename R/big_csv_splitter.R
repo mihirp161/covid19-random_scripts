ae0bb0e3e7e16b6876cf1661251bc33af02d3b40
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-17 09:04:23 -0400

    Add files via upload

---
+++ R/big_csv_splitter.R
@@ -0,0 +1,26 @@
+# SAIL Labs
+# ~Mihir
+
+## Description:
+##-------------
+#* Splits huge csv into multiple parts based on n rows.
+##-------------
+
+# R method to make multiple csvs based on Rows.
+
+library(tidyverse)
+
+#Read in the original csv file
+bigdf_data <- readr::read_csv("xyz.csv") 
+
+# Here I want 60000 rows per file until last row is reached
+# this method forces the execution from innermost, outer () opttional
+groups <- (split(bigdf_data, (seq(nrow(bigdf_data))-1) %/% 60000))
+
+#loop through end of each split, and write file with i
+for (i in seq_along(groups)) {
+  write.csv(groups[[i]], paste0("bigdf_data_output_file", i, ".csv")) 
+}
+#Output: all the files will be in the current directory. 
+
+#EOF
