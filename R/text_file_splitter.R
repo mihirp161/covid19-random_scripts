8148f8848e2255542d76e7631af5688ddaab33a2
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-17 09:05:12 -0400

    Add files via upload

--- R/text_file_splitter.R
+++ R/text_file_splitter.R
@@ -1,12 +1,19 @@
-#For USF SAIL
-#~Mihir
+# SAIL Labs
+# ~Mihir
+
+## Description:
+##-------------
+#* You can use this file to cut N rows from text files.
+##-------------
+
+
 library(NCmisc)
 options(scipen = 9999)
 
 #goes together with python file text_file_gatherer.py, duplicate_check_remove.R
 
 # first read all the files in USC month folder (Give the path to folder, ending /)
-setwd('./2020-03/')
+setwd('./path_to_text_files/')
 
 #filenames
 files <- list.files( pattern="*txt$")
