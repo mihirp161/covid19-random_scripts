1ed23e6a6b45d2dce696addaeb14c67abcf490e1
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-19 15:34:56 -0400

    Add files via upload

--- R/maryTablesUSC_reply.R
+++ R/maryTablesUSC_reply.R
@@ -1,26 +1,25 @@
-# FOR USF SAIL
-# Dre, Mihir
+# SAIL Labs
+# ~Dre, Mihir
+
+## Description:
+##-------------
+#* Same as other tables, this one gets a batch file with json/l inputs and converts them to dataframe.
+#* Then we capture only the reply objects and make csvs with them.
+##-------------
 
 options(scipen = 999999)
-#install.packages("dplyr",repos='http://cran.us.r-project.org')
-library("dplyr")
-#install.packages("readr",repos='http://cran.us.r-project.org')
-library("readr")
-#install.packages("stringr",repos='http://cran.us.r-project.org')
-library("stringr")
-#install.packages("tidyselect",repos='http://cran.us.r-project.org')
-library("tidyselect")
-#install.packages("ndjson", repos='http://cran.us.r-project.org')
-library("ndjson")#, lib.loc="/tmp/RtmpDG5v7Q/downloaded_packages")
-#install.packages("doParallel")
-#library("doParallel")
-library("data.table")
-# creates a list of all json files in dir as a full string
-library("tibble")
+library(dplyr)
+library(readr)
+library(stringr)
+library(tidyselect)
+library(ndjson)
+library(data.table)
+library(tibble)
+library(purrr)
 
 #------------------------------------------- File paths and destination paths ---------------------------------#
 args <- commandArgs(trailingOnly = FALSE)
-fileNames_all <- args[6] #list.files("/shares_bgfs/si_twitter/Dred-MPColab/Mary_files_April_jsons/", "*.jsonl$", full.names = FALSE)
+fileNames_all <- args[6] 
 fileLocation <- fileNames_all
 
 print(paste0("Reading.....", fileLocation))
@@ -31,19 +30,12 @@ endLocation_pre <- sapply(endLocation_pre,tail, 1) #access the last elemnt of li
 print(paste0("true file name: ", endLocation_pre))
 
 #-------------------------------- Read in the json/l files ---------------------------------------------------#
-#fileNames_all[i]
+
 #read the json here and make whatever
 message("starting...")
 parsedTweets <- ndjson::stream_in(fileLocation, cls="dt") %>% dplyr::filter(lang == "en")
-#parsedTweets2 <- ndjson::stream_in("/Users/dre/Downloads/coronaVirus/demo/2020-05-29-04.json", cls="dt") %>% dplyr::filter(lang == "en")
 
-# data <- files %>%
-#         map_df(~fromJSON(file.path(path, .), flatten = TRUE))
-# Remove all the NA's in the dataframe
-#parsedTweets <- gsub("NA", "", parsedTweets)
 parsedTweets[parsedTweets == "NA"]  <- ""
-#na.omit(parsedTweets)
-
 
 #------------------------------ Get only the distict columns ------------------------------------------------#
 #singly columns, put additional ones in the furture
@@ -74,12 +66,14 @@ replyData <- distinct_dat %>%
                             retweeted_status.in_reply_to_screen_name,
                             retweeted_status.in_reply_to_user_id_str)
 
-replyDataLoc <- "/shares_bgfs/si_twitter/covid19/USC_tables/reply_to/" #change your too DreD
+replyDataLoc <- "/path_to_reply_directory/" #change yours too DreD
 
-endLocation_pre <- sub('\\..*$', '', endLocation_pre) #get before .json
+endLocation_pre <- sub('\\..*$', '', endLocation_pre) #gets before .json
 
 readr::write_excel_csv(x = replyData, path = paste0(replyDataLoc,endLocation_pre, "_replyTable.csv"))
 
 print(paste0("Finished with: ",endLocation_pre))
 rm(list=ls())
 quit()
+
+#EOF
