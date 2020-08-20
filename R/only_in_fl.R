4f9f1d3c12aec587c4604d3a644bb64ef78372f3
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2020-08-20 09:56:27 -0400

    Add files via upload

--- R/only_in_fl.R
+++ R/only_in_fl.R
@@ -1,23 +1,19 @@
-# SAIL Labs
-# ~Mihir
-
-## Description:
-##-------------
-#* Reads bunch of csvs and only keeps the users who are from Florida.
-##-------------
-
+#For USF SAIL
+# ~M
 options(scipen = 999)
 
 library(data.table)
 library(readr)
+library(dplyr)
 
 #------------------------ read the files for location ------------------------------------------
-setwd('./file_path/')
+#setwd('/shares_bgfs/si_twitter/Dred-MPColab/MIHIR PERSONAL TEMP/locationr/july/')
+setwd('./locationr/')
 
 #filenames
 files <- list.files( pattern="*.csv$")
 
-#read
+#read only columns that we want
 temp <- lapply(files, function(x) readr::read_csv(x))
 
 loco_data <- data.table::rbindlist(temp, fill = T) #make a df
@@ -29,18 +25,27 @@ setwd('..') #goes back once
 #change the column names
 colnames(loco_data) <- c("id_str", "place.full_name")
 
-#reduced the data.table to Fl, and Florida only
-loco_subbed <- loco_data[Reduce(`|`, Map(`%like%`, list(place.full_name), c(', FL', 'Florida, '))),]
+#reduced the data.table to Fl, and Florida only ---- This is for Florida only tweet
+#loco_subbed <- loco_data[Reduce(`|`, Map(`%like%`, list(place.full_name), c(', FL', 'Florida, '))),]
+
+
+# get all the state abbrevioations and full name, make them up so they match 
+x<- c(paste0(", ",state.abb), paste0(state.name,", "), ", USA")
+
+# check if they match to full names ---- This is for USA only tweets
+loco_subbed <- loco_data[Reduce(`|`, Map(`%like%`, list(place.full_name), x)),]
+
 
 rm(loco_data)
 #------------------------ read the files for queried csv ------------------------------------------
 
-setwd('./file_path/')
+#setwd('/work_bgfs/m/mkpatel/SCRIPT/filtered_new_query_mask/july/')
+setwd('./tweetr/')
 
 #filenames
 files <- list.files( pattern="*.csv$")
 
-#read
+#read only columns that we want
 temp <- lapply(files, function(x) readr::read_csv(x))
 
 filter_data <- data.table::rbindlist(temp, fill = T) #make a df
@@ -54,6 +59,4 @@ setwd('..') #goes back once
 tuc_mary_csv <- merge(filter_data, loco_subbed, by="id_str")
 
 #then write the data to file
-readr::write_excel_csv(tuc_mary_csv, "florida_data.csv")
-
-#EOF
+readr::write_excel_csv(tuc_mary_csv, "july_mask_fl.csv")
