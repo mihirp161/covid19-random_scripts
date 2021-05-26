b079a4886ccde46772a8cc12ca36bad94c5e538c
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2021-05-25 22:52:49 -0400

    Add files via upload

--- R/dr_yu_resubmission_4.R
+++ R/dr_yu_resubmission_4.R
@@ -1,5 +1,6 @@
-options(scipen = 9999999)
+options(scipen = 9999999, digits = 22)
 library(tidyverse)
+library(bit64)
 
 setwd('./done')
 
@@ -10,6 +11,8 @@ for(i in file_names){
   assign(i,  readr::read_csv(i))
 }
 
+
+#------ uncomment incase we need the march files
 # filtered_df <- dplyr::bind_rows(plandemic_data_march.csv, plandemic_data_april.csv,
 #                              plandemic_data_may.csv, plandemic_data_june.csv,
 #                              plandemic_data_july.csv, plandemic_data_august.csv)
@@ -18,6 +21,18 @@ for(i in file_names){
 # 
 # readr::write_excel_csv(filtered_df %>% dplyr::select(-Screenid), "plandemic_data_march_august_updated.csv")
 
+
+# dplyr::n_distinct(tbl$user.id_str)
+# dplyr::n_distinct(tbl$user.screen_name)
+# 
+# dplyr::n_distinct(tbl$retweeted_status.user.id_str)
+# dplyr::n_distinct(tbl$retweeted_status.user.screen_name)
+
+# tbl <- list.files(pattern = "*.csv") %>% 
+#   purrr::map_df(~read_csv(.))
+
+#------------
+
 filtered_df <- dplyr::bind_rows(plandemic_data_may.csv, plandemic_data_june.csv,
                              plandemic_data_july.csv, plandemic_data_august.csv)
 
@@ -25,6 +40,12 @@ setwd("..")
 
 readr::write_excel_csv(filtered_df %>% dplyr::select(-Screenid), "plandemic_data_may_august_updated.csv")
 
+
+readr::write_csv(filtered_df %>% 
+                   dplyr::mutate(Screenid= bit64::as.integer64(Screenid)) %>%
+                   dplyr::select(Screenid) %>%
+                   dplyr::distinct(),
+                       "plandemic_Screenid.csv")
 
 
 filtered_df <- tibble::add_column(filtered_df, Date_std= as.POSIXct(strptime(filtered_df$Date,
@@ -38,6 +59,7 @@ filtered_df <- tibble::add_column(filtered_df, Source_joined_std =  as.POSIXct(s
                                                                                  "%a %b %d %H:%M:%S +0000 %Y",
                                                                                  tz = "GMT")),
                                .after = 9)
+
 
 #check how many unique users are there, because that's how many user will need to have
 dplyr::n_distinct(filtered_df$Screenid)
@@ -69,33 +91,68 @@ for(i in file_names){
 }
 setwd("..")
 
-bots_df <- dplyr::bind_rows(drYu_ds3_may_bots.csv, drYu_ds3_june_bots.csv,
-                             drYu_ds3_july_bots.csv, drYu_ds3_august_bots.csv) %>%
-              dplyr::select(user_screen_name,
-                            user_user_data_id_str,
-                            cap_english,
-                            cap_universal) %>%
-              dplyr::mutate(user_screen_name = gsub("@","",user_screen_name))  %>%
-              dplyr::distinct()
+#------------- not intrested in botometer atm
 
-final_df <- dplyr::left_join(tally_part, user_part,  by= c("Screenid", "Screenname"))
+# bots_df <- dplyr::bind_rows(drYu_ds3_may_bots.csv, drYu_ds3_june_bots.csv,
+#                              drYu_ds3_july_bots.csv, drYu_ds3_august_bots.csv) %>%
+#               dplyr::select(user_screen_name,
+#                             user_user_data_id_str,
+#                             cap_english,
+#                             cap_universal) %>%
+#               dplyr::mutate(user_screen_name = gsub("@","",user_screen_name))  %>%
+#               dplyr::distinct()
+# 
+# setwd('./BOT_CSV_USC_yu')
+# 
+# botmeter_results <- list.files(pattern = "*.csv") %>% 
+#                       purrr::map_df(~read_csv(.))
+# 
+# setwd('..')
 
-final_df <- dplyr::left_join(final_df, bots_df, by= c("Screenid"= "user_user_data_id_str", 
-                                                      "Screenname"="user_screen_name")) %>%
-              select(User_name= Screenname,
-                     User_id= Screenid,
-                     Botometer_cap_english= cap_english,
-                     Botometer_cap_universal= cap_universal,
+# final_df <- dplyr::left_join(tally_part, user_part,  by= c("Screenid", "Screenname"))
+# 
+# final_df <- dplyr::left_join(final_df, bots_df, by= c("Screenid"= "user_user_data_id_str", 
+#                                                       "Screenname"="user_screen_name")) %>%
+#               select(User_name= Screenname,
+#                      User_id= Screenid,
+#                      Botometer_cap_english= cap_english,
+#                      Botometer_cap_universal= cap_universal,
+#                      Twitter_account_created= Source_joined_std,
+#                      Number_of_followers= Source_followers,
+#                      Number_of_followees= Source_following,
+#                      Plandemic_engagement_May= May,
+#                      Plandemic_engagement_June= June,
+#                      Plandemic_engagement_July= July,
+#                      Plandemic_engagement_August= August)#%>%
+#               #tidyr::drop_na(cap_english, cap_universal)
+
+#---------------
+
+
+final_df <- dplyr::left_join(tally_part, user_part,  by= c("Screenname")) %>%
+              dplyr::select(User_name= Screenname,
+                     User_id= Screenid.y,
                      Twitter_account_created= Source_joined_std,
                      Number_of_followers= Source_followers,
                      Number_of_followees= Source_following,
                      Plandemic_engagement_May= May,
                      Plandemic_engagement_June= June,
                      Plandemic_engagement_July= July,
-                     Plandemic_engagement_August= August)#%>%
-              #tidyr::drop_na(cap_english, cap_universal)
+                     Plandemic_engagement_August= August)
 
 
-readr::write_excel_csv(final_df, "Plandemic_Twitter_attributes_051221_file.csv")
+readr::write_excel_csv(final_df, "Plandemic_Twitter_attributes_051221_file_updated_Screenname_joined.csv")
 
+final_df <- dplyr::left_join(tally_part, user_part,  by= c("Screenid", "Screenname")) %>%
+              dplyr::select(User_name= Screenname,
+                            User_id= Screenid,
+                            Twitter_account_created= Source_joined_std,
+                            Number_of_followers= Source_followers,
+                            Number_of_followees= Source_following,
+                            Plandemic_engagement_May= May,
+                            Plandemic_engagement_June= June,
+                            Plandemic_engagement_July= July,
+                            Plandemic_engagement_August= August)
+
+readr::write_excel_csv(final_df, "Plandemic_Twitter_attributes_051221_file_updated_Screenname_Screenid_joined.csv")
 
