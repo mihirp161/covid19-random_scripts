65f46a262999ad3edbf6e2ba580717669c2cef9f
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2021-07-22 19:48:27 -0400

    Update output_csv_fixer.R

--- R/output_csv_fixer.R
+++ R/output_csv_fixer.R
@@ -8,27 +8,29 @@
 ##-------------
 
 
+options(scipen = 9999999, digits = 22)
 library(data.table)
 library(dplyr)
+library(bit64)
 
 #------------------------------------------- file reading and collapsing ----------------------------------------------------
 
 # first read all the files in BOT_CSV folder (Give the path to folder, ending /)
-setwd('./BOT_CSV/')
+setwd('./batch1_restapi_data_botoresults/')
 
 #filenames
 files <- list.files( pattern="*.csv$")
 
 #read only columns that we want
-temp <- lapply(files, function(x) data.table::fread(x, select = c("user_screen_name")))
+temp <- lapply(files, function(x) data.table::fread(x, select = c("user_user_data_id_str")))
 botcsv_data <- data.table::rbindlist(temp, fill = T) #make a df
 
 #create a directory
 setwd('..') #goes back once
-dir.create("./BOT_CSV_OLD/") #(Give path )
+dir.create("./batch1_restapi_data_botoresults_OLD/") #(Give path )
 
 #----------------------------------- use this portion to rename files into current directory, if moving is done-------------
-setwd('./BOT_CSV/')
+setwd('./batch1_restapi_data_botoresults/')
 #rename the file with date
 file.rename(from = files, to = paste0(format(file.mtime(files),"%Y-%m-%d"),"_",files)) #in linux, do file.info(files)$mtime
 #--------------------------------------------------------------------------------------------------------------------------
@@ -37,15 +39,15 @@ files <- list.files( pattern="*.csv$")
 
 #copy all the exports file to a new folder name them accordingly to prevent deletion and overwritinh
 file.copy(files,
-          to = "../BOT_CSV_OLD/", recursive = T,
+          to = "../batch1_restapi_data_botoresults_OLD/", recursive = T,
           overwrite = T, copy.mode = T, copy.date = F)
 setwd('..')
 
 #----------------------------------------------- rename all the files that were copied -----------------------------------------
 #rename the files based on direcotry
-setwd('./BOT_CSV_OLD/')
+setwd('./batch1_restapi_data_botoresults_OLD/')
 
-folder<-"./BOT_CSV_OLD/"
+folder<-"./batch1_restapi_data_botoresults_OLD/"
 
 files<-list.files( pattern="*.csv$")
 
@@ -60,13 +62,13 @@ message("Please don't delete the BOT_CSV_OLD directory, it contains the work you
 #--------------------------------------------- remake outputcsv file so you can carry on ---------------------------------------------
 
 #read
-outputcsv_file <- readr::read_csv("output.csv") #in linux, just to read.csv("output.csv",stringsAsFactors = F)
-colnames(botcsv_data) <- "user_screen_name"
+outputcsv_file <- data.table::fread("CH_rest_API_unique_userids.csv", select = c("Screenid")) #in linux, just do read.csv("output.csv",stringsAsFactors = F)
+colnames(botcsv_data) <- "Screenid"
 #now compare the two frame and remove the rows which are in 
 
-outputcsv_file <- dplyr::anti_join(outputcsv_file,botcsv_data, by= "user_screen_name")
+outputcsv_file <- dplyr::anti_join(outputcsv_file,botcsv_data, by= c("Screenid"))
 
 #write the file back
-readr::write_csv(outputcsv_file, "output.csv") #in linux, do write.csv("output.csv")
+data.table::fwrite(outputcsv_file, "new_output.csv") #in linux, do write.csv("new_output.csv")
 
 #EOF
