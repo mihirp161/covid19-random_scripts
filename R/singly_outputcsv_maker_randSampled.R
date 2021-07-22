10b060d1a2c6a422183b47db2d3acd6d9776af0b
Mihir Patel <47681434+opendatasurgeon@users.noreply.github.com>
2021-07-21 20:14:42 -0400

    Update singly_outputcsv_maker_randSampled.R

--- R/singly_outputcsv_maker_randSampled.R
+++ R/singly_outputcsv_maker_randSampled.R
@@ -1,9 +1,17 @@
+options(scipen = 9999999, digits = 22)
+library(dplyr)
+library(readxl)
+library(readr)
+library(bit64)
+
+
+
 # SAIL Labs
 # ~Mihir
 
 ## Description:
 ##-------------
-#* This file will read all the csv, select the user.screen_name and keeps the unique usernames only.
+#* This file will read all the csv, select the Screenid and keeps the unique userids only.
 #*  Then you can specify people and number of rows you want to keep in each file. It is meant to
 #*  help with botometer.
 ##-------------
@@ -12,37 +20,31 @@
 library(data.table)
 
 # first read all the files in a folder (Give the path to folder (....), ending /)
-setwd('./path_to_files/')
+setwd('./path_to_files_here/')
 
 #filenames
 files <- list.files( pattern="*csv$")
 
 #read only columns that we want
-temp <- lapply(files, function(x) data.table::fread(x, select = c("user.screen_name")))
+temp <- lapply(files, function(x) data.table::fread(x, select = c("Screenid")))
 data <- data.table::rbindlist(temp, fill = T) #make a df
 
 
-#remove the duplicates usernames
-n_original <- data[!duplicated(data$user.screen_name), ]
+#remove the duplicates userids
+n_original <-  data[!duplicated(data$Screenid), ]
 
-#sample 100K with probablity sample without replacements
+#sample df with probablity sample without replacements
 set.seed(786)
-rand_data <- data.frame(user.screen_name= sample(n_original$user.screen_name, 100000, replace = F), stringsAsFactors = F)
+rand_data <- data.frame(Screenid= sample( bit64::as.integer64(n_original$Screenid), nrow(n_original), replace = F), stringsAsFactors = F)
 
 
-# write a file for letter
-data.table::fwrite(rand_data, "original_user_name_file.csv")
 
-# Here I want 20000 rows per file until last row is reached
+# Here I want ~2500 rows per file until last row is reached
 # this method forces the execution from innermost, outer () opttional
-groups <- (split(rand_data, (seq(nrow(rand_data))-1) %/% 20000))
+groups <- (split(rand_data, (seq(nrow(rand_data))-1) %/% 2537))
 people <- c("1_user", "2_user", "3_user", "4_user", "5_user")
 
 #loop through end of each split, and write file with i
 for (i in seq_along(groups)) {
-  write.csv(groups[[i]], paste0("output_", people[i], ".csv")) 
+  readr::write_csv(groups[[i]], paste0("output_", people[i], ".csv")) 
 }
-
-
-#all files would be in the current directory which got set all the way above.
-#EOF
